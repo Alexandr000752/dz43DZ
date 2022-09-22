3a32de52153854df4392f1f66e6f0356a5de40d9
Alexandr000752 <112328813+Alexandr000752@users.noreply.github.com>
2022-09-22 19:21:44 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите b1: ");
+double numberB1 = double.Parse(Console.ReadLine());
+
+Console.Write("Введите k1: ");
+double numberK1 = double.Parse(Console.ReadLine());
+
+Console.Write("Введите b2: ");
+double numberB2 = double.Parse(Console.ReadLine());
+
+Console.Write("Введите k2: ");
+double numberK2 = double.Parse(Console.ReadLine());
+
+double Xisk = (numberB1 - numberB2)/(numberK2 - numberK1);
+double Yisk = numberK1*Xisk + numberB1;
+
+Console.WriteLine("Точка пересечения М(" + Xisk + "; " + Yisk + ")");
+
+
+
